416ea66c46de381e7dcd5728f94f062fa6d80ebe
Vignesh-Manogaran <vickymanob16@gmail.com>
2025-09-06 08:11:50 +0530

    Update StockDetailPage.tsx

--- src/pages/StockDetailPage.tsx
+++ src/pages/StockDetailPage.tsx
@@ -697,8 +697,7 @@ const StockDetailPage: React.FC = () => {
                         <p className="text-sm font-bold text-yellow-900">
                           {stockAnalysis.financialHealth.valuation[
                             "Dividend Yield"
-                          ]?.value?.toFixed(1) || "1.2"
-                          %
+                          ]?.value?.toFixed(1) || "1.2"}%
                         </p>
                       </div>
                     </div>
@@ -724,8 +723,7 @@ const StockDetailPage: React.FC = () => {
                         <p className="text-sm font-bold text-emerald-900">
                           {stockAnalysis.financialHealth.profitability.ROCE?.value?.toFixed(
                             1
-                          ) || "7.5"
-                          %
+                          ) || "7.5"}%
                         </p>
                       </div>
                       <div
@@ -747,8 +745,7 @@ const StockDetailPage: React.FC = () => {
                         <p className="text-sm font-bold text-emerald-900">
                           {stockAnalysis.financialHealth.profitability.ROE?.value?.toFixed(
                             1
-                          ) || "14.4"}
-                          %
+                          ) || "14.4"}%
                         </p>
                       </div>
                     </div>
